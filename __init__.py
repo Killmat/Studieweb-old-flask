92f53b03a919035d886530d06bd815465784bb62
killmat <mathias@thusholt.dk>
2016-10-31 10:00:36 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from config import ss_dir
+from os import listdir
+from os.path import isfile, join
+
+app = Flask(__name__)
+
+
+images_folder = ss_dir
+images = [f for f in listdir(images_folder) if isfile(join(images_folder, f))]
+images.sort()
+images.reverse()
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', images=images)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ config.py
@@ -0,0 +1,1 @@
+ss_dir = '/var/www/Studieweb/Studieweb/static/screenshot'
