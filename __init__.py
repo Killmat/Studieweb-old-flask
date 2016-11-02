770e47baa07fa84237d997a34f77b1c4967039b1
mathias <mathias@thusholt.dk>
2016-11-02 13:23:32 +0100

    Init /about /docs, improvements to mobile.

--- __init__.py
+++ __init__.py
@@ -13,9 +13,34 @@ images.reverse()
 
 ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a orci nisi. Suspendisse quis tempus leo. Ut luctus nunc lacinia arcu tincidunt sodales. Duis felis massa, mollis eget enim in, porta iaculis arcu. Nam ut arcu auctor, elementum arcu nec, vulputate dolor. Duis mattis pharetra euismod. Quisque interdum nisi quam, id ultrices nibh sagittis ac. Nulla scelerisque molestie lacus, quis interdum leo mattis a. Nunc non nisl lorem.'
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html', title='Mathias Thusholt', ipsum=ipsum)
+    return render_template (
+        'index.html',
+        title='Mathias Thusholt',
+        ipsum=ipsum,
+        index_active='active'
+    )
+
+
+@app.route('/about')
+def about():
+    return render_template (
+        'about.html',
+        title='Om mig',
+        about_active='active'
+    )
+
+
+@app.route('/docs')
+def docs():
+    return render_template(
+        'docs.html',
+        title='Dokumentation',
+        docs_active='active'
+    )
 
 
 @app.route('/screenshot')
